b5b48214d43aa6af61675949208537ac037f1863
GiGiDiSi <subs.ggds@gmail.com>
2026-07-14 23:15:09 +0800

    Updated Menu and Aspect Ratio

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -6,11 +6,9 @@ const GAME_SCENE_PATH := "res://Scenes/VN_Interface_Demo.tscn"
 
 func _on_new_game_button_pressed() -> void:
 	get_tree().change_scene_to_file(GAME_SCENE_PATH)
-
-
-func _on_continue_button_pressed() -> void:
 	# TEST
 	print("Continue pressed - no save system wired up yet")
+
 
 
 func _on_settings_button_pressed() -> void:
